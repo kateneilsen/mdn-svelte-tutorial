332eb85082eefc33e9622e8f0b00f940c07ad5a4
Kate <neilsenk09@gmail.com>
2023-09-12 11:55:15 -0400

    filter todos

--- 03-adding-dynamic-behavior/src/components/Todos.svelte
+++ 03-adding-dynamic-behavior/src/components/Todos.svelte
@@ -9,16 +9,31 @@
   $: completedTodos = todos.filter((todo) => todo.completed).length;
 
   let newTodoName = "";
+  let newTodoId;
+  $: {
+    if (totalTodos === 0) {
+      newTodoId = 1;
+    } else {
+      newTodoId = Math.max(...todos.map((t) => t.id)) + 1;
+    }
+  }
 
   function addTodo() {
-    todos = [...todos, { id: 999, name: newTodoName, completed: false }];
+    todos = [...todos, { id: newTodoId, name: newTodoName, completed: false }];
     newTodoName = "";
   }
 
   function removeTodo(todo) {
     todos = todos.filter((t) => t.id !== todo.id);
   }
-  // $: console.log("newTodoName: ", newTodoName);
+
+  let filter = "all";
+  const filterTodos = (filter, todos) =>
+    filter === "active"
+      ? todos.filter((t) => !t.completed)
+      : filter === "completed"
+      ? todos.filter((t) => t.completed)
+      : todos;
 </script>
 
 <!-- Todos.svelte -->
@@ -43,17 +58,32 @@
 
   <!-- Filter -->
   <div class="filters btn-group stack-exception">
-    <button class="btn toggle-btn" aria-pressed="true">
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "all"}
+      aria-pressed={filter === "all"}
+      on:click={() => (filter = "all")}
+    >
       <span class="visually-hidden">Show</span>
       <span>All</span>
       <span class="visually-hidden">tasks</span>
     </button>
-    <button class="btn toggle-btn" aria-pressed="false">
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "active"}
+      aria-pressed={filter === "active"}
+      on:click={() => (filter = "active")}
+    >
       <span class="visually-hidden">Show</span>
       <span>Active</span>
       <span class="visually-hidden">tasks</span>
     </button>
-    <button class="btn toggle-btn" aria-pressed="false">
+    <button
+      class="btn toggle-btn"
+      class:btn__primary={filter === "completed"}
+      aria-pressed={filter === "completed"}
+      on:click={() => (filter = "completed")}
+    >
       <span class="visually-hidden">Show</span>
       <span>Completed</span>
       <span class="visually-hidden">tasks</span>
@@ -68,7 +98,7 @@
   <!-- Todos -->
   <!-- To-dos -->
   <ul role="list" class="todo-list stack-large" aria-labelledby="list-heading">
-    {#each todos as todo (todo.id)}
+    {#each filterTodos(filter, todos) as todo (todo.id)}
       <li class="todo">
         <div class="stack-small">
           <div class="c-cb">
